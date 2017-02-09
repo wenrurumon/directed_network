68276b7b2109c6fd011528418e3422527bee467b
進撃の雯雯老師 <askahu6@gmail.com>
2017-02-09 16:42:52 -0600

    Update path_summary.R

--- connection_1701/path_summary.R
+++ connection_1701/path_summary.R
@@ -1,5 +1,5 @@
 
-rm(list=ls())
+# rm(list=ls())
 library(igraph)
 library(WGCNA)
 library(grplasso)
@@ -71,6 +71,36 @@ summary_signalnet <- function(g){
        sum(sapply(temp,function(x){length(x$ends)}))/sum(sapply(pathwaymap,length)))
 }
 
+summary_signalnet2 <- function(mat){
+  g <- graph_from_adjacency_matrix(t(mat))
+  mat3 <- mat2 <- matrix(0,nrow(mat),ncol(mat),dimnames=dimnames(mat))
+  temp <- list()
+  for(i in 1:length(pathwaymap)){
+    pathi <- unique(pathwaymap[[i]])[!grepl(names(pathwaymap)[i],unique(pathwaymap[[i]]))]
+    if(length(pathi)==0){next}
+    node.from <- V(g)[grep(names(pathwaymap)[i],names(V(g)))]
+    node.to <- V(g)[grep(paste(pathwaymap[[i]],collapse="|"),names(V(g)))]
+    paths_all <- all_simple_paths(g,node.from,node.to)
+    for(pathj in paths_all){
+      for(j in 1:(length(pathj)-1)){
+        mat2[grep(names(pathj[j+1]),colnames(mat2)),grep(names(pathj[j]),colnames(mat2))] <- 1
+      }
+    }
+    paths_short <- all_shortest_paths(g,node.from,node.to)$res
+    paths_short <- paths_short[sapply(paths_short,length)>1]
+    for(pathj in paths_short){
+      for(j in 1:(length(pathj)-1)){
+        mat3[grep(names(pathj[j+1]),colnames(mat3)),grep(names(pathj[j]),colnames(mat3))] <- 1
+      }
+    }    
+    temp[[i]] <- list(paths_all=paths_all,paths_short=paths_short,ends=unique(sapply(paths_short,function(x){x[length(x)]})))
+  }
+  dp <- sum(sapply(temp,function(x){length(x$ends)}))/sum(sapply(pathwaymap,length))
+  fdr_all <- (sum(mat)-sum(mat2))/sum(mat)
+  fdr_short <- (sum(mat)-sum(mat3))/sum(mat)
+  list(path_summary=temp,g.all=mat2,g.short=mat3,kpi=c(dp=dp,fdr_all=fdr_all,fdr_short=fdr_short))
+}
+
 g <- graph_from_adjacency_matrix(t(sem_final_signal[[1]]));sum(t(sem_final_signal[[1]]));summary_signalnet(g)
 g <- graph_from_adjacency_matrix(t(rlt_p2pfinal[[1]][[18]][,1:24]));sum(rlt_p2pfinal[[1]][[18]][,1:24]);summary_signalnet(g)
 g <- graph_from_adjacency_matrix(t(rlt_p2pfinal[[2]]));summary_signalnet(g)
@@ -78,7 +108,8 @@ g <- graph_from_adjacency_matrix(t(rlt_p2pfinal[[2]]));summary_signalnet(g)
 load('pathwaymap.rda')
 temp <- t(rlt_p2pfinal[[2]])
 g <- graph_from_adjacency_matrix(temp)
-temp <- rownames(rlt_p2pfinal[[2]])
+# temp <- rownames(rlt_p2pfinal[[2]])
+temp <- rownames(rlt_p2pfinal[[1]][[18]])
 pathwaymap <- pathwaymap[names(pathwaymap)%in%temp]
 pathwaymap <- lapply(pathwaymap, function(x){unique(x[x%in%temp])})
 summary_signalnet(g)
@@ -135,7 +166,6 @@ plotnet(score_grplasso,'undirected')
 #Random DAG simulation
 #########################
 
-
 # source('http://bioconductor.org/biocLite.R')
 # biocLite(c('graph','RBGL','Rgraphviz'))
 
@@ -145,7 +175,7 @@ randomDAGmat <- function(n,p){
 }
 
 sum(rlt_p2pinp[[2]][[18]][,1:24])
-set.seed(12345); randmat <- lapply(1:10000,function(i){randomDAGmat(24,0.18)})
+set.seed(12345); randmat <- lapply(1:100,function(i){randomDAGmat(24,0.18)})
 randmat <- lapply(randmat,function(x){
   dimnames(x) <- dimnames(rlt_p2pinp[[2]][[18]])
   return(x)
@@ -164,8 +194,39 @@ summary_signalnet <- function(g){
        sum(sapply(pathwaymap,length)),
        sum(sapply(temp,function(x){length(x$ends)}))/sum(sapply(pathwaymap,length)))
 }
+summary_signalnet2 <- function(mat){
+  g <- graph_from_adjacency_matrix(t(mat))
+  mat3 <- mat2 <- matrix(0,nrow(mat),ncol(mat),dimnames=dimnames(mat))
+  temp <- list()
+  for(i in 1:length(pathwaymap)){
+    pathi <- unique(pathwaymap[[i]])[!grepl(names(pathwaymap)[i],unique(pathwaymap[[i]]))]
+    if(length(pathi)==0){next}
+    node.from <- V(g)[grep(names(pathwaymap)[i],names(V(g)))]
+    node.to <- V(g)[grep(paste(pathwaymap[[i]],collapse="|"),names(V(g)))]
+    paths_all <- all_simple_paths(g,node.from,node.to)
+    for(pathj in paths_all){
+      for(j in 1:(length(pathj)-1)){
+        mat2[grep(names(pathj[j+1]),colnames(mat2)),grep(names(pathj[j]),colnames(mat2))] <- 1
+      }
+    }
+    paths_short <- all_shortest_paths(g,node.from,node.to)$res
+    paths_short <- paths_short[sapply(paths_short,length)>1]
+    for(pathj in paths_short){
+      for(j in 1:(length(pathj)-1)){
+        mat3[grep(names(pathj[j+1]),colnames(mat3)),grep(names(pathj[j]),colnames(mat3))] <- 1
+      }
+    }    
+    temp[[i]] <- list(paths_all=paths_all,paths_short=paths_short,ends=unique(sapply(paths_short,function(x){x[length(x)]})))
+  }
+  dp <- sum(sapply(temp,function(x){length(x$ends)}))/sum(sapply(pathwaymap,length))
+  fdr_all <- (sum(mat)-sum(mat2))/sum(mat)
+  fdr_short <- (sum(mat)-sum(mat3))/sum(mat)
+  list(path_summary=temp,g.all=mat2,g.short=mat3,kpi=c(dp=dp,fdr_all=fdr_all,fdr_short=fdr_short))
+}
 
+# i <- 0
 test <- sapply(randmat,function(x){
+  # print(i<<-i+1)
   g <- graph_from_adjacency_matrix(t(x))
   rlt <- try(summary_signalnet(g))
   if(!is.list(rlt)) return(NA)
@@ -182,4 +243,51 @@ test <- cbind(test,test[,2]/test[,1])
 head(test)
 par(mfrow=c(1,2))
 hist(test[-1,4],main = '#aligned paths/#kegg paths'); hist(test[-1,5],main='#aligned paths/#all paths detected')
+
+#################################
+# DAG with other methodologies
+#################################
+
+# sort(cenet,T)[49*2]
+# adj <- (cenet>=7)
+# adj2 <- do.call(rbind,lapply(1:nrow(adj),function(i){cbind(subpath(adj[i,],max.parent = 2),i)}))
+# cost <- mape(adj2,signal_score)
+# coe_dag <- ip_zy(adj,adj2,cost)
+
+##############################
+#Bayesian Network
+library(bnlearn)
+# datExpr <- do.call(cbind,lapply(signal_score,function(x) x[,1]))[,1:10]
+datExpr <- do.call(cbind,signal_score)
+system.time(bn.hc <- hc(as.data.frame(datExpr)))
+bnhc3 <- bn.hc[[3]]
+bnnet <- matrix(0,nrow=nrow(TOM),ncol=ncol(TOM),dimnames=dimnames(TOM))
+for(i in 1:nrow(bnhc3)){
+  bnnet[grep(bnhc3[i,2],rownames(bnnet)),grep(bnhc3[i,1],colnames(bnnet))] <- 1
+}
+bnnet <- apply(apply(bnnet,1,function(x){tapply(x,rep(names(signal_score),sapply(signal_score,ncol)),sum)}),1,function(x){tapply(x,rep(names(signal_score),sapply(signal_score,ncol)),sum)})
+diag(bnnet) <- 0
+for(i in 1:nrow(bnnet)){
+  for(j in 1:ncol(bnnet)){
+    if(bnnet[i,j]>bnnet[j,i]){
+      bnnet[j,i] <- 0
+    } else {
+      bnnet[i,j] <- 0
+    }
+  }
+}
+# plotnet(bnnet>=sort(bnnet[bnnet>0],T)[49])
+plot(g.bnnet <- graph_from_adjacency_matrix(t(bnnet>=sort(bnnet[bnnet>0],T)[49])))
+summary_signalnet(g.bnnet)
+sum(bnnet>=sort(bnnet[bnnet>0],T)[49])
+
+#####################################
+# Validation
+#####################################
+
+vali_om <- summary_signalnet2(rlt_p2pinp[[2]][[18]][,1:24])$kpi
+vali_bn <- summary_signalnet2(bnnet>=sort(bnnet[bnnet>0],T)[49])$kpi
+vali_rand <- lapply(randmat[-1],function(x){summary_signalnet2(x)$kpi})
+
+##################################
 
