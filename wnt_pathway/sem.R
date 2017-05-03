76d728ef5e49c31d126b378f42201e56a7f10088
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-03 15:41:39 -0500

    Create sem.R

---
+++ wnt_pathway/sem.R
@@ -0,0 +1,26 @@
+rm(list=ls())
+setwd('C:\\Users\\zhu2\\Documents\\signaling\\codes\\')
+source('sparse_2sem_final.R')
+source('local_cnif_macro.R')
+source('CNIF.R')
+sourceCpp("score_function_regression.cpp")
+sourceCpp("simple_cycle.cpp")
+sourceCpp("initial_sem.cpp")
+setwd('C:\\Users\\zhu2\\Documents\\getpathway')
+load("~/getpathway/gene39761.rda.RData")
+load('C:/Users/zhu2/Documents/getpathway/model20170215/rlt_p2pinp.rda')
+library(flare)
+library(grplasso)
+library(data.table)
+library(dplyr)
+library(igraph)
+
+# node filtered
+rlt.jr <- as.matrix(read.table('clipboard',header=T))
+
+# sem dag
+expr <- expinpath$`Wnt signaling pathway`
+expr <- expr[,colnames(expr)%in%as.vector(rlt.jr)]
+gc();sem <- sparse_2sem(expr,times=100,lambda=0.5)
+sum(sem[[1]]>0.8)
+gc();dag <- CNIF(data=expr,init.adj=(sem[[1]]>0.8),max_parent=3)
