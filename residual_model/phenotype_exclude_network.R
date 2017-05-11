c22d0ff7e6ec7bf09a06c351e81b253ae4348d28
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-10 22:58:13 -0500

    Update phenotype_exclude_network.R

--- residual_model/phenotype_exclude_network.R
+++ residual_model/phenotype_exclude_network.R
@@ -1,33 +1,122 @@
 
+
 rm(list=ls())
-load("C:/Users/zhu2/Documents/getpathway/model20170215/summary/phenet.rda")
 
-data <- phenet$data
-adj <- phenet$adj
-colnames(adj) <- rownames(adj)
-adj <- dplyr::filter(reshape::melt(adj),value==1)
-colnames(adj)[1:2] <- c('to','from')
+#cca
 
-themodel <- function(iy,ix,data){
-  data <- data.matrix(data)
-  y <- data[,iy]
-  if(length(ix)==0){
-    res <- y
-  } else {
-    x <- data[,ix,drop=F]
-    res <- lm(y~x)$residual
+p_ginv_sq <- function(X,p){
+  X.eigen = eigen(X);
+  X.rank = sum(X.eigen$values>1e-8);
+  X.value = X.eigen$values[1:X.rank]^(-1*p);
+  if (length(X.value)==1){
+    D = as.matrix(X.value);
+  }else{
+    D = diag(X.value);
   }
-  res <- cbind(res)
-  colnames(res) <- colnames(data)[iy]
-  res
+  rlt = X.eigen$vectors[,1:X.rank] %*% D %*% t(X.eigen$vectors[,1:X.rank]);
+  return(rlt);
 }
+mrank <- function(X){
+  X.svd = svd(X);
+  X.rank = sum(X.svd$d>1e-6);
+  return(X.rank);
+}
+mrank_sq <- function(X){
+  X.eigen = eigen(X);
+  X.rank = sum(Re(X.eigen$values)>1e-6);
+  return(X.rank);
+}
+CCA_chisq_test <- function(rho,n,p,q){
+  tstat = -1*n*sum(log(1-rho^2));
+  p_value = pchisq(tstat,(p*q),lower.tail=FALSE);
+  return(p_value);          
+}
+cca <- function(A,B){
+  n = nrow(A);
+  p = mrank(A);
+  q = mrank(B);
+  if (p <= q){
+    X = A;
+    Y = B;
+  }else{
+    X = B;
+    Y = A;
+  }
+  R = p_ginv_sq(cov(X),0.5) %*% cov(X,Y) %*% p_ginv_sq(cov(Y),1) %*% cov(Y,X) %*% p_ginv_sq(cov(X),0.5);
+  k = mrank_sq(R);
+  d = Re(eigen(R)$values);
+  rho = d[1:k]^(0.5);
+  rho[rho >= 0.9999]=0.9;
+  chisq_p = CCA_chisq_test(rho,n,p,q);
+  return(c("chisq_p"=chisq_p,"df"=p*q));
+}
+ccap <- function(A,B){as.numeric(cca(A,B)[1])}
+ccap2 <- function(A,B){
+	out <- try(ccap(A,B))
+	if(is.numeric(out)){
+		return(out)
+	} else {
+		return(NA)
+	}}
 
-p <- colnames(data)
-pdata.res <- lapply(p,function(pi){
-  iy <- which(p==pi)
-  px <- adj[which(adj[,1]==pi),2]
-  ix <- which(p%in%px)
-  themodel(iy,ix,data)
+#candidate
+setwd('/home/zhu/rushdata/residual_model')
+for(i in dir()){load(i)}
+
+#snpdata
+setwd('/home/zhu/rushdata/tacc/cut')
+files <- dir(pattern='snp')
+snpdata <- lapply(files,function(x){
+	load(x)
+	return(x)
 })
-names(pdata.res) <- sapply(pdata.res,colnames)
-save(pdata.res,file='pdata_res.rda')
+snpdata <- do.call(c,snpdata)
+names(snpdata) <- paste0('s',names(snpdata))
+
+#methylation data
+setwd('/home/zhu/rushdata/tacc/cut')
+files <- dir(pattern='m')
+methydata <- lapply(files,function(x){
+	load(x)
+	return(x)
+})
+methydata <- do.call(c,methydata)
+names(methydata) <- paste0('m',names(methydata))
+
+#candidate
+candidates <- unique(c(df_m2eres[,2],df_s2eres[,2],df_s2mres[,2]))
+alldata <- c(snpdata,methydata)
+alldata <- alldata[names(alldata)%in%candidates]
+
+#pheres
+setwd('/home/zhu/rushdata/tacc')
+load('pdata_res.rda')
+pid <- rownames(methydata[[1]])
+pdata.res <- lapply(pdata.res,function(x){
+	x[match(pid,rownames(x)),,drop=F]
+})
+pheres <- do.call(cbind,pdata.res)
+
+#candidates step1
+i <- 0
+can1 <- sapply(alldata,function(x){
+	print(i<<-i+1)
+	ccap2(x,pheres)
+})
+alldata <- alldata[can1<=0.05]
+
+#step2
+i <- 0
+test <- sapply(pdata.res,function(x){
+	print(i<<-i+1)
+	sapply(alldata,function(y){
+		ccap2(x,y)
+		})
+	})
+test <- reshape::melt(test)
+colnames(test)[1:2] <- c('from','to')
+df_ms2pres <- test[test$value < 0.05/length(test$value),]
+df_ms2pres <- cbind(to=paste(df_ms2pres[,2]),from=paste(df_ms2pres[,1]))
+
+setwd('/home/zhu/rushdata/residual_model')
+save(df_ms2pres,file='df_ms2pres.rda')
